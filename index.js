fa0f7748fa0caba81b94e57ddbc33ae44714398f
Jeremy Marube <jeremymarube1@gmail.com>
2025-06-27 14:52:10 +0300

    has-target-sum

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
+// hasTargetSum.js
+
+/**
+ * Returns true if any two numbers in the array add up to the target.
+ * @param {number[]} array - Array of integers
+ * @param {number} target - Target sum
+ * @returns {boolean}
+ */
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Create a set to keep track of numbers we've seen
+  const seenNumbers = new Set();
+
+  // Iterate through each number in the array
+  for (const num of array) {
+    // Calculate the complement that would add up to the target
+    const complement = target - num;
+
+    // If the complement exists in the set, we've found a pair
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+
+    // Otherwise, add the current number to the set and keep looking
+    seenNumbers.add(num);
+  }
+
+  // If no pairs add up to the target, return false
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+// Test cases
 if (require.main === module) {
-  // add your own custom tests in here
+  
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +43,19 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([0, -1, 2, 9], 1));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([5], 5));
 }
+
+module.exports = hasTargetSum;
+
 
 module.exports = hasTargetSum;
